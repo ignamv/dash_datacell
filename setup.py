7466bc9e2ed8cac01ca7806c6fad82a8b1715487
Ignacio Martinez <imartinezvazquez@allegromicro.com>
2018-11-20 11:41:18 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dash_datacell',
+    version='0.1',
+    py_modules=['dash_datacell'],
+    install_requires=['dash'],
+    author='Ignacio Martinez',
+    author_email='ignamv@gmail.com',
+    license='GPLv3',
+    keywords='dash',
+    url='https://github.com/ignamv/dash_datacell'
+)
